102fef3edc409f0bc6c24b3c184c1b83633736a0
广泽 <haofu@91jkys.com>
2021-02-03 15:22:04 +0800

    ADD: tail数据功能基本ok

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,95 @@
-use bytes::buf::BufMut;
 use bytes::Bytes;
 use bytes::BytesMut;
 use io::Result;
-use log::{info, trace, warn};
-use memchr::memchr;
-use std::cell::RefCell;
-use std::future::Future;
-use std::marker::PhantomData;
-use std::marker::PhantomPinned;
-use std::pin::Pin;
-use std::task::Context;
-use std::task::Poll;
-use std::{borrow::BorrowMut, io::SeekFrom};
+use log::warn;
+use memchr::memrchr_iter;
+use std::io::SeekFrom;
 use tokio::fs::File;
-use tokio::io::BufReader;
-use tokio::io::ReadBuf;
-use tokio::io::{self, AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncSeekExt};
-use tokio::sync::mpsc;
-use tokio::sync::mpsc::Receiver;
-use tokio::sync::mpsc::Sender;
-use tokio::time::sleep;
+use tokio::io::{self, AsyncReadExt, AsyncSeekExt};
+use tokio::time::{Duration, Instant};
+use std::future::Future;
 
-const FLUSH_MILI_SECONDS: u8 = 1;
-const FLUSH_BATCH_BYTES: u32 = 64 * 1024;
+const FLUSH_SECONDS: u64 = 1;
+const FLUSH_BATCH_BYTES: usize = 64 * 1024;
+const NEW_LINE_TERMINATOR: char = '\n';
+
+type FlushRemote<T> =  fn(Bytes) -> T;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut file = pop_file().await?;
-    let delimeter: u8 = b'\n';
+    let file = pop_file().await?;
+    tokio::spawn(async move { loop_tail_new_log_data(file, flush_data)});
+    return Ok(());
+}
 
+async fn loop_tail_new_log_data<T: Future>(mut file: File, cbk: FlushRemote<T>) -> io::Result<()> {
     // Seek to end of file
     let meta = file.metadata().await?;
-    file.seek(SeekFrom::Start(meta.len())).await?;
 
     // Every time read one line data
     //
     // Batch cutting logic
     // 1. Time exceed and buf has at lease one line, 1 second
-    // 2. Batch is bigger than 64KB, flush.
+    // 2. Batch is bigger than 64KB and there is several line data.
     //
     // Overceed size line
-    // If one line's size overceed 32kb, force cutting off this line
+    // If one line is very long, we will cut off the line, skip to another line
     //
     // Buffer channel
     // for backpressure
-    let mut read_bytes = 0;
-    let mut buf = BytesMut::with_capacity(1024 * 64);
+    let mut read_bytes: usize = 0;
+    let mut buf = BytesMut::with_capacity(FLUSH_BATCH_BYTES);
+    let mut instant = Instant::now();
 
+    file.seek(SeekFrom::Start(meta.len())).await?;
     loop {
-        if read_bytes > 32 * 1024 {
-            // seek last \n
-            // flush bytes from start to last \n
-            // if there is no
-            flush_data(&buf).await;
+        if read_bytes > FLUSH_BATCH_BYTES || instant.elapsed() > Duration::from_secs(FLUSH_SECONDS)
+        {
+            // seek last \n from back to first
+            // flush bytes from start to last '\n' idx
+            // this iterator will reverse find element
+            let mut iterator = memrchr_iter(NEW_LINE_TERMINATOR as u8, &buf);
+            match iterator.next() {
+                //  Have found no \n, maybe there is a very long line
+                Some(idx) => {
+                    let afterwards_buf = buf.split_off(idx);
+                    let byts = buf.freeze();
+                    // todo backpressure logic here
+                    let _ = cbk(byts).await;
+                    buf = afterwards_buf;
+
+                    instant = Instant::now();
+                    read_bytes = 0;
+                }
+                None => {
+                    // continue to read, every time in the loop we check if we can find a new \n
+                    warn!("we have met a very long line, continue to find \n ");
+                }
+            }
         }
         let size = file.read_buf(&mut buf).await?;
         read_bytes += size;
     }
-    return Ok(());
 }
 
-async fn flush_data(bytes: &BytesMut) {}
+// todo
+// usually we use this method to send to remote peer
+async fn flush_data(bytes: Bytes) {
+    println!("start to flush data to remote peer, data is {:?}", bytes);
+}
 
 async fn pop_file() -> Result<File> {
     return Ok(File::open("test.txt").await?);
 }
 
+#[cfg(test)]
 mod tests {
-    #[cfg(test)]
     use bytes::BytesMut;
     use std::future::Future;
-    #[cfg(test)]
-    use std::io::SeekFrom;
-    #[cfg(test)]
     use tokio::fs::File;
-    #[cfg(test)]
-    use tokio::io::{AsyncReadExt, AsyncSeekExt};
+    use tokio::io::{AsyncSeekExt, AsyncWriteExt};
+    use tokio::time::sleep;
+    use core::time::Duration;
 
     fn call_once<T: Future>(f: T) -> T::Output {
         let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
@@ -83,17 +97,17 @@ mod tests {
     }
 
     #[test]
-    fn test_bytes_mut_multiple_read_buf() {
+    fn test_loop_write_line_data_every_second() {
         call_once(async {
-            if let Ok(mut file) = File::open("./test_case.txt").await {
-                if let Ok(meta) = file.metadata().await {
-                    let mut buf = BytesMut::with_capacity(10);
+            // loop write new data to 
+            tokio::spawn(async {
+                if let Ok(mut file) = File::open("./test/test_case.txt").await {
                     for _ in 0..10 {
-                        let size = file.read_buf(&mut buf).await;
-                        println!("{:?}, buf:{:?}", size, buf);
+                        let _ = file.write_all(b"new line test data!").await;
+                        sleep(Duration::from_secs(1)).await;
                     }
                 }
-            }
-        })
+            });
+        });
     }
 }
